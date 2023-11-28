1a04659ef80b393dffe75af361753313efe8456b
kykileur <kyliannfernandes@gmail.com>
2023-11-28 15:10:12 +0100

    symfony 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,18 +1,24 @@
 <?php
 // src/Controller/ProgramController.php
+// src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index(): Response
-    {
-        return new Response(
-            '<html><body>Wild Series Index</body></html>'
-        );
-    }
+    #[Route('/', name: 'index')]
+   public function index(): Response
+   {
+       // return render twig
+   }
+
+   #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'list')]
+   public function show(int $id): Response
+  {
+    return $this->render('show.html.twig',['id'=>$id]);
+  }
 }
